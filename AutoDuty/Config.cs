773af9d1d1dde3d96722fc33617cf3aeb5f776db
Adam Bennett <adam-bennett@live.com>
2024-02-29 21:47:59 -0700

    ver bump

--- AutoDuty/Config.cs
+++ AutoDuty/Config.cs
@@ -7,7 +7,7 @@ namespace AutoDuty;
 [Serializable]
 public class Configuration : IPluginConfiguration
 {
-    public int Version { get; set; } = 4;
+    public int Version { get; set; } = 5;
 
     public bool AutoDuty { get; set; } = true;
 
