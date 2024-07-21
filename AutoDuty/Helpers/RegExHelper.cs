d7e40f0d92d73ac39a9de927eea410a5c414b0d7
Simon Latusek <Simon.Latusek@gmail.com>
2024-07-21 17:09:47 +0200

    highlight w2w

--- AutoDuty/Helpers/RegExHelper.cs
+++ AutoDuty/Helpers/RegExHelper.cs
@@ -7,10 +7,15 @@ namespace AutoDuty.Helpers
         [GeneratedRegex(@"([^<]*)?(?><?([0-9\. ]*\,[0-9\. ]*\,[0-9\. ]*)>([^<]*)<\/>)?", RegexOptions.CultureInvariant)]
         public static partial Regex ColoredTextRegex();
 
-        [GeneratedRegex(@"(\()([0-9]{3,4})(\))( 「W2W-まとめ」)?(.*)(\.json)", RegexOptions.CultureInvariant)]
+        [GeneratedRegex($@"(\()([0-9]{{3,4}})(\))( {PathIdentifiers.W2W})?(.*)(\.json)", RegexOptions.CultureInvariant)]
         public static partial Regex PathFileRegex();
 
         [GeneratedRegex(@"([0-9]{3,})", RegexOptions.CultureInvariant)]
         public static partial Regex InteractionObjectIdRegex();
+    }
+
+    public static class PathIdentifiers
+    {
+        public const string W2W = @"「W2W-まとめ」";
     }
 }
