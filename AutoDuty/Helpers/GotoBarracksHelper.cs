58c6a7951ebc46a5b0d4bbe0f6fdaaa67d38f6f5
Herculezz <adam-bennett@live.com>
2024-07-23 10:17:03 -0700

    Added Uldah Barracks exit dataids thx Jigain

--- AutoDuty/Helpers/GotoBarracksHelper.cs
+++ AutoDuty/Helpers/GotoBarracksHelper.cs
@@ -34,7 +34,7 @@ namespace AutoDuty.Helpers
 
         internal static bool GotoBarracksRunning = false;
         internal static uint BarracksTerritoryType(uint _grandCompany) => _grandCompany == 1 ? 536u : (_grandCompany == 2 ? 534u : 535u);
-        internal static uint ExitBarracksDoorDataId(uint _grandCompany) => _grandCompany == 1 ? 2007528u : (_grandCompany == 2 ? 2006963u : 0u);
+        internal static uint ExitBarracksDoorDataId(uint _grandCompany) => _grandCompany == 1 ? 2007528u : (_grandCompany == 2 ? 2006963u : 2007530u);
         private static IGameObject? barracksDoorGameObject = null;
         private static Vector3 barracksDoorLocation => UIState.Instance()->PlayerState.GrandCompany == 1 ? new Vector3(98.00867f, 41.275635f, 62.790894f) : (UIState.Instance()->PlayerState.GrandCompany == 2 ? new Vector3(-80.00789f, -0.5001702f, -6.6672616f) : new Vector3(-153.30743f, 5.2338257f, -98.039246f));
         private static uint _barracksDoorDataId => UIState.Instance()->PlayerState.GrandCompany == 1 ? 2007527u : (UIState.Instance()->PlayerState.GrandCompany == 2 ? 2006962u : 0u);
