e536938d2dabcdaee2f2f89f101a16fff2a696f9
Adam Bennett <adam-bennett@live.com>
2024-07-30 11:46:50 -0700

    Add conditional to trigger for SchedulerHelper

--- AutoDuty/Helpers/SchedulerHelper.cs
+++ AutoDuty/Helpers/SchedulerHelper.cs
@@ -12,7 +12,9 @@ namespace AutoDuty.Helpers
 
             internal Action Action { get; set; } = () => { };
 
-            internal int TimeMS { get; set; }
+            internal int TimeMS { get; set; } = 0;
+
+            internal Func<bool>? Condition { get; set; } = null;
 
             internal bool RunOnce { get; set; } = true;
         }
@@ -21,16 +23,19 @@ namespace AutoDuty.Helpers
 
         internal static bool ScheduleAction(string name, Action action, int timeMS, bool runOnce = true) => schedules.Add(new Schedule() { Name = name, Action = action, TimeMS = Environment.TickCount + timeMS, RunOnce = runOnce });
 
+        internal static bool ScheduleAction(string name, Action action, Func<bool> condition, bool runOnce = true) => schedules.Add(new Schedule() { Name = name, Action = action, Condition = condition, RunOnce = runOnce });
+
         internal static int DescheduleAction(string name) => schedules.RemoveWhere(s => s.Name == name);
 
         internal static void ScheduleInvoker(IFramework _)
         {
             foreach (var schedule in schedules)
             {
-                if (Environment.TickCount >= schedule.TimeMS)
+                if (schedule.TimeMS != 0 ? Environment.TickCount >= schedule.TimeMS : schedule.Condition?.Invoke() ?? false)
                 {
                     schedule.Action();
-                    schedules.Remove(schedule);
+                    if (schedule.RunOnce)
+                        schedules.Remove(schedule);
                 }
             }
         }
