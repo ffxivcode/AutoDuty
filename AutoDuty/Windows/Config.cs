d64a20579e3615f1fecada004d38a0a85c39f878
Adam Bennett <adam-bennett@live.com>
2024-08-06 23:30:08 -0700

    v111

--- AutoDuty/Windows/Config.cs
+++ AutoDuty/Windows/Config.cs
@@ -19,7 +19,7 @@ public class Configuration : IPluginConfiguration
 {
     public HashSet<string> DoNotUpdatePathFiles { get; set; } = [];
 
-    public int Version { get; set; } = 110;
+    public int Version { get; set; } = 111;
     public int AutoRepairPct { get; set; } = 50;
     public int AutoGCTurninSlotsLeft = 5;
     public int LoopTimes = 1;
