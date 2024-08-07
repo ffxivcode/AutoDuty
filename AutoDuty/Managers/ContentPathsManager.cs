491bcc4cee22da97d3c89c0678b149364564941b
Simon Latusek <Simon.Latusek@gmail.com>
2024-08-07 15:00:51 +0200

    invalid paths getting removed from selection + fix for the one broken one.

--- AutoDuty/Managers/ContentPathsManager.cs
+++ AutoDuty/Managers/ContentPathsManager.cs
@@ -12,6 +12,9 @@ using AutoDuty.Windows;
 
 namespace AutoDuty.Managers
 {
+    using System;
+    using ECommons.DalamudServices;
+
     internal static class ContentPathsManager
     {
         internal static Dictionary<uint, ContentPathContainer> DictionaryPaths = [];
@@ -96,12 +99,14 @@ namespace AutoDuty.Managers
                 Match pathMatch = RegexHelper.PathFileRegex().Match(this.FileName);
 
                 string pathFileColor = AutoDuty.Plugin.Configuration.DoNotUpdatePathFiles.Contains(this.FileName) ? ImGuiHelper.pathFileColorNoUpdate : ImGuiHelper.pathFileColor;
-
+                this.id = uint.Parse(pathMatch.Groups[2].Value);
                 this.ColoredNameString = pathMatch.Success ?
                                              $"{pathMatch.Groups[1]}{ImGuiHelper.idColor}{pathMatch.Groups[2]}</>{pathMatch.Groups[3]}<0.8,0.8,1>{pathMatch.Groups[4]}</>{pathFileColor}{pathMatch.Groups[5]}</><0.5,0.5,0.5>{pathMatch.Groups[6]}</>" :
                                              this.FileName;
                 this.ColoredNameRegex = RegexHelper.ColoredTextRegex().Match(this.ColoredNameString);
             }
+
+            public uint id;
 
             public string Name     { get; }
             public string FileName { get; }
@@ -118,34 +123,44 @@ namespace AutoDuty.Managers
                 {
                     if (this.pathFile == null)
                     {
-                        this.RevivalFound = false;
-                        string json;
-
-                        using (StreamReader streamReader = new(this.FilePath, Encoding.UTF8)) 
-                            json = streamReader.ReadToEnd();
-
-                        // Backwards compatibility, with instant updating
-                        if (!json.Contains("\"createdAt\""))
+                        try
                         {
-                            List<string>? paths;
-                            if ((paths = JsonSerializer.Deserialize<List<string>>(json)) != null)
+                            this.RevivalFound = false;
+                            string json;
+
+                            using (StreamReader streamReader = new(this.FilePath, Encoding.UTF8))
+                                json = streamReader.ReadToEnd();
+
+                            // Backwards compatibility, with instant updating
+                            if (!json.Contains("\"createdAt\""))
                             {
-                                this.pathFile         = PathFile.Default;
-                                this.pathFile.actions = paths.ToArray();
+                                List<string>? paths;
+                                if ((paths = JsonSerializer.Deserialize<List<string>>(json)) != null)
+                                {
+                                    this.pathFile         = PathFile.Default;
+                                    this.pathFile.actions = paths.ToArray();
+                                }
+
+                                string jsonNew = JsonSerializer.Serialize(this.PathFile, BuildTab.jsonSerializerOptions);
+                                File.WriteAllText(this.FilePath, jsonNew);
+                            }
+                            else
+                            {
+                                this.pathFile = JsonSerializer.Deserialize<PathFile>(json);
                             }
 
-                            string jsonNew = JsonSerializer.Serialize(this.PathFile, BuildTab.jsonSerializerOptions);
-                            File.WriteAllText(this.FilePath, jsonNew);
+                            foreach (string action in this.PathFile.actions)
+                                if (action.Split('|')[0].Trim() == "Revival")
+                                {
+                                    this.RevivalFound = true;
+                                    break;
+                                }
                         }
-                        else
-                            this.pathFile = JsonSerializer.Deserialize<PathFile>(json);
-
-                        foreach (string action in this.PathFile.actions)
-                            if (action.Split('|')[0].Trim() == "Revival")
-                            {
-                                this.RevivalFound = true;
-                                break;
-                            }
+                        catch (Exception ex)
+                        {
+                            Svc.Log.Info($"{this.FilePath} is not a valid duty path");
+                            DictionaryPaths[this.id].Paths.Remove(this);
+                        }
                     }
 
                     return this.pathFile!;
