c7f6ea801b48f0f35429187a889247d3f532ec6e
Adam Bennett <adam-bennett@live.com>
2024-08-29 15:41:43 -0700

    v138

--- AutoDuty/Windows/Config.cs
+++ AutoDuty/Windows/Config.cs
@@ -28,7 +28,7 @@ namespace AutoDuty.Windows;
 public class Configuration : IPluginConfiguration
 {
     //Meta
-    public int Version { get => 137; set { } }
+    public int Version { get => 138; set { } }
     public HashSet<string> DoNotUpdatePathFiles = [];
     public Dictionary<uint, Dictionary<Job, int>> PathSelections = [];
 
