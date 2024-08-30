a76da3400b9633f29b8c5d80b676efac455bc0fc
Adam Bennett <adam-bennett@live.com>
2024-08-29 22:05:36 -0700

    More AutoPublish Version Automate Fix Null TaskManager on Unload

--- AutoDuty/Windows/Config.cs
+++ AutoDuty/Windows/Config.cs
@@ -28,7 +28,7 @@ namespace AutoDuty.Windows;
 public class Configuration : IPluginConfiguration
 {
     //Meta
-    public int Version { get => 138; set { } }
+    public int Version { get; set; }
     public HashSet<string> DoNotUpdatePathFiles = [];
     public Dictionary<uint, Dictionary<Job, int>> PathSelections = [];
 
