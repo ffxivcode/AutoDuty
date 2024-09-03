0b81851189ad8d70f390a85e8585e34bcae5125c
Adam Bennett <adam-bennett@live.com>
2024-09-03 16:27:40 -0700

    Moved DutySupport to new QueueHelper

--- AutoDuty/Helpers/QueueHelper.cs
+++ AutoDuty/Helpers/QueueHelper.cs
@@ -14,12 +14,14 @@ namespace AutoDuty.Helpers
 {
     internal unsafe static class QueueHelper
     {
-        internal static void Invoke(Content? content)
+        internal static void Invoke(Content? content, DutyMode dutyMode)
         {
-            if (State != ActionState.Running && content != null)
+            if (State != ActionState.Running && content != null && dutyMode != DutyMode.None)
             {
+                _dutyMode = dutyMode;
                 _content = content;
-                Svc.Log.Info($"Queueing: {content.Name}");
+                Svc.Log.Info($"Queueing: {dutyMode}: {content.Name}");
+                AutoDuty.Plugin.Action = $"Queueing {_dutyMode}: {content.Name}";
                 State = ActionState.Running;
                 AutoDuty.Plugin.States |= PluginState.Other;
                 Svc.Framework.Update += QueueUpdate;
@@ -30,49 +32,69 @@ namespace AutoDuty.Helpers
         {
             Svc.Framework.Update -= QueueUpdate;
             if (State == ActionState.Running)
-                Svc.Log.Info($"Done Queueing: {_content?.Name}");
+                Svc.Log.Info($"Done Queueing: {_dutyMode}: {_content?.Name}");
             _content = null;
             State = ActionState.None;
             AutoDuty.Plugin.States &= ~PluginState.Other;
-            _allConditionsMetToJoin = false;
+            _allConditionsMetToJoin = false; 
+            _dutyMode = DutyMode.None;
         }
 
         internal static ActionState State = ActionState.None;
 
         private static Content? _content = null;
+        private static DutyMode _dutyMode = DutyMode.None;
         private static AddonContentsFinder* _addonContentsFinder = null;
         private static bool _allConditionsMetToJoin = false;
 
-        internal static void QueueUpdate(IFramework _)
+        private static bool ContentsFinderConfirm()
         {
-            if (_content == null || AutoDuty.Plugin.InDungeon || Svc.ClientState.TerritoryType == _content?.TerritoryType)
-            {
-                Svc.Framework.Update -= QueueUpdate;
-                Stop();
-                return;
-            }
-
-            if (!EzThrottler.Throttle("QueueHelper", 250))
-                return;
-
-            AutoDuty.Plugin.Action = $"Queueing Duty: {_content!.Name}";
-
-            if (!ObjectHelper.IsValid)
-                return;
-
             if (GenericHelpers.TryGetAddonByName("ContentsFinderConfirm", out AtkUnitBase* addonContentsFinderConfirm) && GenericHelpers.IsAddonReady(addonContentsFinderConfirm))
             {
                 Svc.Log.Debug("Queue Helper - Confirming DutyPop");
                 AddonHelper.FireCallBack(addonContentsFinderConfirm, true, 8);
-                return;
+                return true;
             }
+            return false;
+        }
 
-            if (Conditions.IsInDutyQueue)
+        internal static void QueueSupport()
+        {
+            if (!GenericHelpers.TryGetAddonByName("DawnStory", out AtkUnitBase* addonDawnStory) || !GenericHelpers.IsAddonReady(addonDawnStory))
             {
-                Svc.Log.Debug("Queue Helper - InDutyQueue");
+                Svc.Log.Debug("Queue Helper - Opening Dawn Support");
+                AgentModule.Instance()->GetAgentByInternalId(AgentId.DawnStory)->Show();
                 return;
             }
 
+            if (addonDawnStory->AtkValues[8].UInt < _content!.ExVersion)
+            {
+                Svc.Log.Debug($"Queue Helper - You do not have expansion: {_content.ExVersion} unlocked stopping");
+                Stop();
+                return;
+            }
+
+            if (addonDawnStory->AtkValues[343].UInt != _content!.ExVersion)
+            {
+                Svc.Log.Debug($"Queue Helper - Opening Expansion: {_content.ExVersion}");
+                AddonHelper.FireCallBack(addonDawnStory, true, 11, _content.ExVersion);
+                return;
+            }
+
+            else if (addonDawnStory->AtkValues[21].UInt != _content.DawnIndex + 1)
+            {
+                Svc.Log.Debug($"Queue Helper - Clicking: {_content.EnglishName} at index: {_content.DawnIndex + addonDawnStory->AtkValues[27].UInt} {addonDawnStory->AtkValues[27].UInt}");
+                AddonHelper.FireCallBack(addonDawnStory, true, 12, (uint)_content.DawnIndex + addonDawnStory->AtkValues[27].UInt);
+            }
+            else
+            {
+                Svc.Log.Debug($"Queue Helper - Clicking: Register For Duty");
+                AddonHelper.FireCallBack(addonDawnStory, true, 14);
+            }
+        }
+
+        private static void QueueRegular()
+        {
             if (ContentsFinder.Instance()->IsUnrestrictedParty != AutoDuty.Plugin.Configuration.Unsynced)
             {
                 Svc.Log.Debug("Queue Helper - Setting UnrestrictedParty");
@@ -82,7 +104,7 @@ namespace AutoDuty.Helpers
 
             if (!_allConditionsMetToJoin && (!GenericHelpers.TryGetAddonByName("ContentsFinder", out _addonContentsFinder) || !GenericHelpers.IsAddonReady((AtkUnitBase*)_addonContentsFinder)))
             {
-                Svc.Log.Debug($"Queue Helper - Opening ContentsFinder to {_content.Name}");
+                Svc.Log.Debug($"Queue Helper - Opening ContentsFinder to {_content!.Name}");
                 AgentContentsFinder.Instance()->OpenRegularDuty(_content.ContentFinderCondition);
                 return;
             }
@@ -96,7 +118,7 @@ namespace AutoDuty.Helpers
                 return;
             vectorDutyListItems.ForEach(pointAtkComponentTreeListItem => listAtkComponentTreeListItems.Add(*(pointAtkComponentTreeListItem.Value)));
 
-            if (!_allConditionsMetToJoin && AgentContentsFinder.Instance()->SelectedDutyId != _content.ContentFinderCondition)
+            if (!_allConditionsMetToJoin && AgentContentsFinder.Instance()->SelectedDutyId != _content!.ContentFinderCondition)
             {
                 Svc.Log.Debug($"Queue Helper - Opening ContentsFinder to {_content.Name} because we have the wrong selection of {listAtkComponentTreeListItems[(int)_addonContentsFinder->SelectedRow].Renderer->GetTextNodeById(5)->GetAsAtkTextNode()->NodeText.ToString().Replace("...", "")}");
                 AgentContentsFinder.Instance()->OpenRegularDuty(_content.ContentFinderCondition);
@@ -104,7 +126,7 @@ namespace AutoDuty.Helpers
             }
 
             var selectedDutyName = _addonContentsFinder->AtkValues[18].GetValueAsString();
-            if (selectedDutyName != _content.Name && !string.IsNullOrEmpty(selectedDutyName))
+            if (selectedDutyName != _content!.Name && !string.IsNullOrEmpty(selectedDutyName))
             {
                 Svc.Log.Debug($"Queue Helper - We have {_addonContentsFinder->SelectedDutyTextNode[0].Value->NodeText.ToString().Replace("...", "")} selected, not {_content.Name}, Clearing");
                 AddonHelper.FireCallBack((AtkUnitBase*)_addonContentsFinder, true, 12, 1);
@@ -124,6 +146,26 @@ namespace AutoDuty.Helpers
                 Svc.Log.Debug("Queue Helper - All Conditions Met, Clicking Join");
                 AddonHelper.FireCallBack((AtkUnitBase*)_addonContentsFinder, true, 12, 0);
                 return;
+            }
+        }
+
+        internal static void QueueUpdate(IFramework _)
+        {
+            if (_content == null || AutoDuty.Plugin.InDungeon || Svc.ClientState.TerritoryType == _content?.TerritoryType)
+                Stop();
+
+            if (!EzThrottler.Throttle("QueueHelper", 250) || !ObjectHelper.IsValid || ContentsFinderConfirm() || Conditions.IsInDutyQueue) return;
+
+            switch (_dutyMode)
+            {
+                case DutyMode.Regular:
+                case DutyMode.Trial:
+                case DutyMode.Raid:
+                    QueueRegular();
+                    break;
+                case DutyMode.Support:
+                    QueueSupport();
+                    break;
             }
         }
 
