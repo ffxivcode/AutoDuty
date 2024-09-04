76eb5724595b29c99878d8354aa261511f1a15c4
Adam Bennett <adam-bennett@live.com>
2024-09-04 09:55:27 -0700

    unused usings

--- AutoDuty/Helpers/QueueHelper.cs
+++ AutoDuty/Helpers/QueueHelper.cs
@@ -7,9 +7,8 @@ using FFXIVClientStructs.FFXIV.Client.Game.UI;
 using FFXIVClientStructs.FFXIV.Client.UI;
 using FFXIVClientStructs.FFXIV.Client.UI.Agent;
 using FFXIVClientStructs.FFXIV.Component.GUI;
-using System;
 using System.Collections.Generic;
-using static AutoDuty.Helpers.ContentHelper;
+
 
 namespace AutoDuty.Helpers
 {
