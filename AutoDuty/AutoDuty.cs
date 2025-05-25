8285005646c17071269e39e7dc310dbbb7090802
Simon Latusek <Simon.Latusek@gmail.com>
2025-05-25 06:38:10 +0200

    stop bm movement during interaction

--- AutoDuty/AutoDuty.cs
+++ AutoDuty/AutoDuty.cs
@@ -228,7 +228,7 @@ public sealed class AutoDuty : IDalamudPlugin
             AssemblyDirectoryInfo = AssemblyFileInfo.Directory;
             
             Version = 
-                ((PluginInterface.IsDev     ? new Version(0,0,0, 215) :
+                ((PluginInterface.IsDev     ? new Version(0,0,0, 217) :
                   PluginInterface.IsTesting ? PluginInterface.Manifest.TestingAssemblyVersion ?? PluginInterface.Manifest.AssemblyVersion : PluginInterface.Manifest.AssemblyVersion)!).Revision;
 
             if (!_configDirectory.Exists)
