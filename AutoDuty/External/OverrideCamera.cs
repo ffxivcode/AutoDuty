a9eecbf08dd65d803546d6fafa355566bcb68bc5
Simon Latusek <Simon.Latusek@gmail.com>
2025-08-06 20:19:52 +0200

    CameraFace update from vnav

--- AutoDuty/External/OverrideCamera.cs
+++ AutoDuty/External/OverrideCamera.cs
@@ -1,38 +1,32 @@
-﻿//Entire file from vnavmesh
+﻿//All from https://github.com/awgil/ffxiv_navmesh/blob/master/vnavmesh/Movement/OverrideCamera.cs.
+
 using Dalamud.Hooking;
 using Dalamud.Utility.Signatures;
-using ECommons.DalamudServices;
-using ECommons.GameHelpers;
 using FFXIVClientStructs.FFXIV.Client.System.Framework;
 using System;
-using System.Numerics;
 using System.Runtime.InteropServices;
 
 namespace AutoDuty.External;
 
+using System.Numerics;
+using ECommons.DalamudServices;
+using ECommons.MathHelpers;
+
 [StructLayout(LayoutKind.Explicit, Size = 0x2B0)]
 public unsafe struct CameraEx
 {
-    [FieldOffset(0x130)] public float DirH; // 0 is north, increases CW
-    [FieldOffset(0x134)] public float DirV; // 0 is horizontal, positive is looking up, negative looking down
-    [FieldOffset(0x138)] public float InputDeltaHAdjusted;
-    [FieldOffset(0x13C)] public float InputDeltaVAdjusted;
-    [FieldOffset(0x140)] public float InputDeltaH;
-    [FieldOffset(0x144)] public float InputDeltaV;
-    [FieldOffset(0x148)] public float DirVMin; // -85deg by default
-    [FieldOffset(0x14C)] public float DirVMax; // +45deg by default
+    [FieldOffset(0x140)] public float DirH; // 0 is north, increases CW
+    [FieldOffset(0x144)] public float DirV; // 0 is horizontal, positive is looking up, negative looking down
+    [FieldOffset(0x148)] public float InputDeltaHAdjusted;
+    [FieldOffset(0x14C)] public float InputDeltaVAdjusted;
+    [FieldOffset(0x150)] public float InputDeltaH;
+    [FieldOffset(0x154)] public float InputDeltaV;
+    [FieldOffset(0x158)] public float DirVMin; // -85deg by default
+    [FieldOffset(0x15C)] public float DirVMax; // +45deg by default
 }
 
 public unsafe class OverrideCamera : IDisposable
 {
-    internal void Face(Vector3 pos)
-    {
-        Enabled = true;
-        SpeedH = SpeedV = 360.Degrees();
-        DesiredAzimuth = Angle.FromDirectionXZ(pos - Player.Object.Position) + 180.Degrees();
-        DesiredAltitude = -30.Degrees();
-    }
-
     public bool Enabled
     {
         get => _rmiCameraHook.IsEnabled;
@@ -52,7 +46,7 @@ public unsafe class OverrideCamera : IDisposable
     public Angle SpeedV = 360.Degrees(); // per second
 
     private delegate void RMICameraDelegate(CameraEx* self, int inputMode, float speedH, float speedV);
-    [Signature("40 53 48 83 EC 70 44 0F 29 44 24 ?? 48 8B D9")]
+    [Signature("48 8B C4 53 48 81 EC ?? ?? ?? ?? 44 0F 29 50 ??")]
     private Hook<RMICameraDelegate> _rmiCameraHook = null!;
 
     public OverrideCamera()
@@ -66,6 +60,14 @@ public unsafe class OverrideCamera : IDisposable
         _rmiCameraHook.Dispose();
     }
 
+    internal void Face(Vector3 pos)
+    {
+        Enabled         = true;
+        SpeedH          = SpeedV = 360.Degrees();
+        DesiredAzimuth  = Angle.FromDirectionXZ(pos - Player.Object.Position) + 180.Degrees();
+        DesiredAltitude = -30.Degrees();
+    }
+
     private void RMICameraDetour(CameraEx* self, int inputMode, float speedH, float speedV)
     {
         _rmiCameraHook.Original(self, inputMode, speedH, speedV);
@@ -76,11 +78,8 @@ public unsafe class OverrideCamera : IDisposable
             var deltaV = (DesiredAltitude - self->DirV.Radians()).Normalized();
             var maxH = SpeedH.Rad * dt;
             var maxV = SpeedV.Rad * dt;
-            //self->InputDeltaH = Math.Clamp(deltaH.Rad, -maxH, maxH);
-            //self->InputDeltaV = Math.Clamp(deltaV.Rad, -maxV, maxV);
-            self->InputDeltaH = deltaH.Rad;
-            self->InputDeltaV = deltaV.Rad;
-            Enabled = false;
+            self->InputDeltaH = Math.Clamp(deltaH.Rad, -maxH, maxH);
+            self->InputDeltaV = Math.Clamp(deltaV.Rad, -maxV, maxV);
         }
     }
 }
